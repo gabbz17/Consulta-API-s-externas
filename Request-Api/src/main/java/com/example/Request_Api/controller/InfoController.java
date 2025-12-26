670540876dfd3de237ae7606cc7b9a6f6b28cdf8
Gabriel Coutinho <coutinho.dev17@gmail.com>
2025-12-26 17:47:34 -0300

    refactor: alter link access api

--- Request-Api/src/main/java/com/example/Request_Api/controller/InfoController.java
+++ Request-Api/src/main/java/com/example/Request_Api/controller/InfoController.java
@@ -31,7 +31,7 @@ public class InfoController {
 
         RestTemplate restTemplate = new RestTemplate();
 
-        ResponseEntity<WeatherResponseDto> response = restTemplate.getForEntity(String.format("https://api.openweathermap.org/data/2.5/weather?q=%s&appid=98a8b73d523a49b8c5a1e6efb2516b97&lang=pt_br", city), WeatherResponseDto.class);
+        ResponseEntity<WeatherResponseDto> response = restTemplate.getForEntity(String.format("https://api.openweathermap.org/data/2.5/weather?q=%s&appid={APIKEY}&lang=pt_br", city), WeatherResponseDto.class);
 
         return response.getBody();
     }
@@ -41,7 +41,7 @@ public class InfoController {
         RestTemplate restTemplate = new RestTemplate();
 
         ResponseEntity<CepResponseDto> response_cep = restTemplate.getForEntity(String.format("https://viacep.com.br/ws/%s/json/", cep), CepResponseDto.class);
-        ResponseEntity<WeatherResponseDto> response_weather = restTemplate.getForEntity(String.format("https://api.openweathermap.org/data/2.5/weather?q=%s&appid=98a8b73d523a49b8c5a1e6efb2516b97&lang=pt_br", response_cep.getBody().localidade()), WeatherResponseDto.class);
+        ResponseEntity<WeatherResponseDto> response_weather = restTemplate.getForEntity(String.format("https://api.openweathermap.org/data/2.5/weather?q=%s&appid={APIKEY}&lang=pt_br", response_cep.getBody().localidade()), WeatherResponseDto.class);
 
 
         return new InfoResponseDto(response_cep.getBody(), response_weather.getBody(), LocalDateTime.now());
