91effa185e71ebf8c03bf39fdffe6afc42061798
Gabriel Coutinho <coutinho.dev17@gmail.com>
2026-01-06 21:28:17 -0300

    refactor: update a structure of controller class

--- Request-Api/src/main/java/com/example/Request_Api/controller/InfoController.java
+++ Request-Api/src/main/java/com/example/Request_Api/controller/InfoController.java
@@ -1,51 +1,37 @@
 package com.example.Request_Api.controller;
 
+import com.example.Request_Api.Service.InfoService;
 import com.example.Request_Api.dto.CepResponseDto;
 import com.example.Request_Api.dto.InfoResponseDto;
 import com.example.Request_Api.dto.WeatherResponseDto;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.client.RestTemplate;
 
-import java.time.LocalDateTime;
 
 @RestController
 @RequestMapping("request")
 public class InfoController {
 
+    private final InfoService service;
+
+    public InfoController(InfoService service) {
+        this.service = service;
+    }
+
     @GetMapping("/cep/{cep}")
     public CepResponseDto getCep(@PathVariable String cep) {
-
-        RestTemplate restTemplate = new RestTemplate();
-
-        ResponseEntity<CepResponseDto> response = restTemplate.getForEntity(String.format("https://viacep.com.br/ws/%s/json/", cep), CepResponseDto.class);
-
-        return response.getBody();
+        return service.getCep(cep);
     }
 
     @GetMapping("/weather/{city}")
     public WeatherResponseDto getWeather(@PathVariable String city) {
-
-        RestTemplate restTemplate = new RestTemplate();
-
-        ResponseEntity<WeatherResponseDto> response = restTemplate.getForEntity(String.format("https://api.openweathermap.org/data/2.5/weather?q=%s&appid={APIKEY}&lang=pt_br", city), WeatherResponseDto.class);
-
-        return response.getBody();
+        return service.getWeather(city);
     }
 
     @GetMapping("/info/{cep}")
     public InfoResponseDto getInfo(@PathVariable String cep) {
-        RestTemplate restTemplate = new RestTemplate();
-
-        ResponseEntity<CepResponseDto> response_cep = restTemplate.getForEntity(String.format("https://viacep.com.br/ws/%s/json/", cep), CepResponseDto.class);
-        ResponseEntity<WeatherResponseDto> response_weather = restTemplate.getForEntity(String.format("https://api.openweathermap.org/data/2.5/weather?q=%s&appid={APIKEY}&lang=pt_br", response_cep.getBody().localidade()), WeatherResponseDto.class);
-
-
-        return new InfoResponseDto(response_cep.getBody(), response_weather.getBody(), LocalDateTime.now());
+        return service.getInfo(cep);
     }
-
-
 }
