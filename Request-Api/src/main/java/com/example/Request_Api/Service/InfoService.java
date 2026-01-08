9703c9932297065dd8af182964796776788de8b9
Gabriel Coutinho <coutinho.dev17@gmail.com>
2026-01-08 20:18:12 -0300

    refactor: update a inject dependency of RestTemplate

--- Request-Api/src/main/java/com/example/Request_Api/Service/InfoService.java
+++ Request-Api/src/main/java/com/example/Request_Api/Service/InfoService.java
@@ -3,6 +3,7 @@ package com.example.Request_Api.Service;
 import com.example.Request_Api.dto.CepResponseDto;
 import com.example.Request_Api.dto.InfoResponseDto;
 import com.example.Request_Api.dto.WeatherResponseDto;
+import org.springframework.context.annotation.Bean;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
@@ -13,25 +14,24 @@ import java.time.LocalDateTime;
 public class InfoService {
 
 
-    private final RestTemplate restTemplate;
-
-    public InfoService(RestTemplate restTemplate) {
-        this.restTemplate = restTemplate;
+    @Bean
+    public RestTemplate restTemplate() {
+        return new RestTemplate();
     }
 
     public CepResponseDto getCep(String cep) {
-        ResponseEntity<CepResponseDto> response = restTemplate.getForEntity(String.format("https://viacep.com.br/ws/%s/json/", cep), CepResponseDto.class);
+        ResponseEntity<CepResponseDto> response = restTemplate().getForEntity(String.format("https://viacep.com.br/ws/%s/json/", cep), CepResponseDto.class);
         return response.getBody();
     }
 
     public WeatherResponseDto getWeather(String city){
-        ResponseEntity<WeatherResponseDto> response = restTemplate.getForEntity(String.format("https://api.openweathermap.org/data/2.5/weather?q=%s&appid={APIKEY}&lang=pt_br", city), WeatherResponseDto.class);
+        ResponseEntity<WeatherResponseDto> response = restTemplate().getForEntity(String.format("https://api.openweathermap.org/data/2.5/weather?q=%s&appid=&lang=pt_br", city), WeatherResponseDto.class);
         return response.getBody();
     }
 
     public InfoResponseDto getInfo(String cep) {
-        ResponseEntity<CepResponseDto> response_cep = restTemplate.getForEntity(String.format("https://viacep.com.br/ws/%s/json/", cep), CepResponseDto.class);
-        ResponseEntity<WeatherResponseDto> response_weather = restTemplate.getForEntity(String.format("https://api.openweathermap.org/data/2.5/weather?q=%s&appid={APIKEY}&lang=pt_br", response_cep.getBody().localidade()), WeatherResponseDto.class);
+        ResponseEntity<CepResponseDto> response_cep = restTemplate().getForEntity(String.format("https://viacep.com.br/ws/%s/json/", cep), CepResponseDto.class);
+        ResponseEntity<WeatherResponseDto> response_weather = restTemplate().getForEntity(String.format("https://api.openweathermap.org/data/2.5/weather?q=%s&appid=&lang=pt_br", response_cep.getBody().localidade()), WeatherResponseDto.class);
         return new InfoResponseDto(response_cep.getBody(), response_weather.getBody(), LocalDateTime.now());
     }
 }
